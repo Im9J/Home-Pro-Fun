326ba587fc93cb99ee19333b93675ccf91d4a5dd
Im9J <88532336+Im9J@users.noreply.github.com>
2021-08-12 22:42:43 +0700

    Create BMI_1.cpp

---
+++ BMI_1.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <Math.h>
+
+int main () {
+    double Weight,Height,BMI;
+    printf("Enter your weight (kg) :");
+    scanf("%lf",&Weight);
+    printf("Enter your Height (m) :");
+    scanf("%lf",&Height);
+    BMI = Weight/pow(Height,2);
+    printf("BMI = %lf\n",BMI);
+
+    if(BMI <= 0){
+        printf("Error");
+    }
+    else if(0 < BMI < 18.5){
+        printf("Underweight");
+    }
+    else if(18.5 <= BMI < 24.9){
+        printf("Normal");
+    }
+    else if(24.9 <= BMI < 29.9){
+        printf("Overweight");
+    }
+    else if(BMI >= 29.9){
+        printf("Obesity");
+    }
+    return 0;
+}
